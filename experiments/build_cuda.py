d0713c5871060c0d3fdaa5c520df52ccc396ea5f
Thomas J. Fan <thomasjpfan@gmail.com>
2024-04-12 15:22:25 -0400

    Update builder

---
+++ experiments/build_cuda.py
@@ -0,0 +1,13 @@
+from flytekit import ImageSpec
+from imagespec_fast_builder import FastImageBuilder
+
+image_spec = ImageSpec(
+    name="flyte_playground",
+    builder="fast-builder",
+    packages=["torch==2.2.2"],
+    registry="ghcr.io/thomasjpfan",
+    # platform="linux/arm64",
+    # cuda="12",
+)
+
+FastImageBuilder()._build_image(image_spec, push=False)
